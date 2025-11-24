12ce9252806668ad681a4d9fae00a7eb36e87bb6
ntsakosurprise <mashelesepru@gmail.com>
2025-11-24 19:12:16 +0200

    chore: remove getWith console.log statements

--- crud/getWith.ts
+++ crud/getWith.ts
@@ -1,6 +1,6 @@
 import {request} from '../request';
 const getWith = async (url: string, config: any, params?: any) => {
-  console.log('getWithConfig', config, url);
+  
   return await request({
     service: url,
     params: !params ? null : params,
